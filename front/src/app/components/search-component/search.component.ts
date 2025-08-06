50f0a0890ad484dc72fd7b565014f6fb26b1c9a0
Douglas Andrade <suportdoug@gmail.com>
2025-08-05 23:13:44 -0300

    Refactor

--- front/src/app/components/search-component/search.component.ts
+++ front/src/app/components/search-component/search.component.ts
@@ -1,32 +1,15 @@
-<<<<<<< Updated upstream
-import { Component, EventEmitter, Output } from '@angular/core';
-=======
 import { Component, EventEmitter, inject, Output } from '@angular/core';
->>>>>>> Stashed changes
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { DataService } from '../../services/data.service';
 
 @Component({
   selector: 'card-search',
-<<<<<<< Updated upstream
   imports: [CommonModule, FormsModule],
-=======
-  standalone: true,
-  imports: [CommonModule, RouterModule, FormsModule],
->>>>>>> Stashed changes
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.css'],
 })
 export class SearchComponent {
-<<<<<<< Updated upstream
-  searchTerm: string = '';
-
-  @Output() search = new EventEmitter<string>();
-
-  onSearchChange() {
-    this.search.emit(this.searchTerm);
-=======
   @Output() search = new EventEmitter<any[]>();
   private readonly dataService = inject(DataService);
 
@@ -51,6 +34,5 @@ export class SearchComponent {
       );
     }
     this.search.emit(this.filteredPaineis);
->>>>>>> Stashed changes
   }
 }
